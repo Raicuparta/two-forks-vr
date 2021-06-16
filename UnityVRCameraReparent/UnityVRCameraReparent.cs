0303414bdeae96587910dc1930d5c1e7fb8867f7
Raicuparta <Raicuparta@users.noreply.github.com>
2021-06-16 22:28:10 +0200

    Prevent vertical rotation

--- UnityVRCameraReparent/UnityVRCameraReparent.cs
+++ UnityVRCameraReparent/UnityVRCameraReparent.cs
@@ -1,4 +1,5 @@
-﻿using MelonLoader;
+﻿using Harmony;
+using MelonLoader;
 using System;
 using System.Reflection;
 using UnityEngine;
@@ -7,6 +8,28 @@ namespace Raicuparta.UnityVRCameraReparent
 {
     public class UnityVRCameraReparent : MelonMod
     {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            HarmonyInstance.Create("Raicuparta.FirewatchVR").PatchAll();
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasInitialized(buildIndex, sceneName);
+
+            var cameraController = GameObject.FindObjectOfType<vgCameraController>();
+            if (!cameraController)
+            {
+                return;
+            }
+
+            cameraController.defaultCameraTuning.ForEach(tuning => {
+                tuning.minVerticalAngle = 0;
+                tuning.maxVerticalAngle = 0;
+            });
+        }
+
         public override void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.F2))
@@ -24,6 +47,37 @@ namespace Raicuparta.UnityVRCameraReparent
             vrCameraParent.SetParent(mainCamera.parent, false);
             mainCamera.SetParent(vrCameraParent);
             vrCameraParent.localPosition = Vector3.down;
+        }
+    }
+
+    [HarmonyPatch(typeof(vgCameraController), "LeanUp")]
+    public class PatchLeanUp
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(vgCameraController), "LeanDown")]
+    public class PatchLeanDown
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(vgCameraController), "LeanVertical")]
+    public class PatchGetRoll
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            MelonLogger.Msg("Prefix");
+            return false;
         }
     }
 }
