b7885b07c4eaf981213d6b999ae7dc838c2f1f51
Raicuparta <Raicuparta@users.noreply.github.com>
2021-06-16 21:09:38 +0200

    Add copied over project files

---
+++ UnityVRCameraReparent/UnityVRCameraReparent.cs
@@ -0,0 +1,29 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Raicuparta.UnityVRCameraReparent
+{
+    public class UnityVRCameraReparent : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                ReparentCamera();
+            }
+        }
+
+        private void ReparentCamera()
+        {
+            MelonLogger.Msg("Reparenting camera");
+
+            var mainCamera = Camera.main.transform;
+            var vrCameraParent = new GameObject().transform;
+            vrCameraParent.SetParent(mainCamera.parent, false);
+            mainCamera.SetParent(vrCameraParent);
+            vrCameraParent.localPosition = Vector3.down;
+        }
+    }
+}
