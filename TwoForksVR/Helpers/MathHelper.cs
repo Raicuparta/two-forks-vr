6df1fff89746aa6bca829a78b054b33f0f274652
Raicuparta <raicuparta@gmail.com>
2021-06-19 17:37:22 +0200

    Refactor math helper

---
+++ TwoForksVR/Helpers/MathHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Raicuparta.TwoForksVR
+{
+    public static class MathHelper
+    {
+        public static float SignedAngle(Vector3 from, Vector3 to, Vector3 axis)
+        {
+            float unsignedAngle = Vector3.Angle(from, to);
+
+            float cross_x = from.y * to.z - from.z * to.y;
+            float cross_y = from.z * to.x - from.x * to.z;
+            float cross_z = from.x * to.y - from.y * to.x;
+            float sign = Mathf.Sign(axis.x * cross_x + axis.y * cross_y + axis.z * cross_z);
+            return unsignedAngle * sign;
+        }
+    }
+}
--- TwoForksVR/VRCompass.cs
+++ TwoForksVR/VRCompass.cs
@@ -12,22 +12,11 @@ namespace Raicuparta.TwoForksVR
         [HarmonyPatch(typeof(vgCompass), "LateUpdate")]
         public class PatchCompass
         {
-            public static float SignedAngle(Vector3 from, Vector3 to, Vector3 axis)
-            {
-                float unsignedAngle = Vector3.Angle(from, to);
-
-                float cross_x = from.y * to.z - from.z * to.y;
-                float cross_y = from.z * to.x - from.x * to.z;
-                float cross_z = from.x * to.y - from.y * to.x;
-                float sign = Mathf.Sign(axis.x * cross_x + axis.y * cross_y + axis.z * cross_z);
-                return unsignedAngle * sign;
-            }
-
             public static bool Prefix(vgCompass __instance, Vector3 ___newRotation, float ___worldOffset)
             {
                 var transform = __instance.transform;
                 var forward = Vector3.ProjectOnPlane(-transform.parent.forward, Vector3.up);
-                var angle = SignedAngle(forward, Vector3.forward, Vector3.up);
+                var angle = MathHelper.SignedAngle(forward, Vector3.forward, Vector3.up);
                 ___newRotation.y = angle - 165f - ___worldOffset;
                 transform.localEulerAngles = ___newRotation;
                 return false;
