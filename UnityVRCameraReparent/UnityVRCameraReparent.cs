49a507e28d31a2235c85aadd87d5a76aa12269df
Raicuparta <raicuparta@gmail.com>
2021-06-19 14:38:08 +0200

    Fix compass rotation

--- UnityVRCameraReparent/UnityVRCameraReparent.cs
+++ UnityVRCameraReparent/UnityVRCameraReparent.cs
@@ -15,6 +15,7 @@ namespace Raicuparta.UnityVRCameraReparent
         static Transform rightHand;
         static Transform leftHand;
         Transform playerBody;
+        static bool isVrInitialized;
 
         public override void OnApplicationStart()
         {
@@ -24,7 +25,10 @@ namespace Raicuparta.UnityVRCameraReparent
 
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
-            VRSettings.enabled = false;
+            if (!isVrInitialized)
+            {
+                VRSettings.enabled = false;
+            }
             base.OnSceneWasInitialized(buildIndex, sceneName);
 
             MelonLogger.Msg("OnSceneWasInitialized");
@@ -96,7 +100,11 @@ namespace Raicuparta.UnityVRCameraReparent
             camera.transform.localPosition = Vector3.zero;
             camera.transform.localRotation = Quaternion.identity;
             camera.nearClipPlane = 0.03f;
-            VRSettings.enabled = true;
+            if (!isVrInitialized)
+            {
+                VRSettings.enabled = true;
+                isVrInitialized = true;
+            }
         }
 
         private void SetUpUI()
@@ -143,7 +151,6 @@ namespace Raicuparta.UnityVRCameraReparent
             var hand = instance.transform;
             hand.SetParent(Camera.main.transform.parent, false);
             var meshRenderer = playerBody.GetComponent<SkinnedMeshRenderer>();
-            MelonLogger.Msg("after finding mesh renderer");
             hand.Find("handModel/hand").GetComponent<MeshRenderer>().material = meshRenderer.materials[2];
 
             if (isLeft)
@@ -170,8 +177,6 @@ namespace Raicuparta.UnityVRCameraReparent
 
         private void ReparentCamera()
         {
-            MelonLogger.Msg("Reparenting camera");
-
             var mainCamera = Camera.main.transform;
             var vrCameraParent = new GameObject("VR Stage").transform;
             vrCameraParent.SetParent(mainCamera.parent, false);
@@ -223,6 +228,59 @@ namespace Raicuparta.UnityVRCameraReparent
             {
                 __instance.OnToss();
                 return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(vgCompass), "LateUpdate")]
+        public class PatchCompass
+        {
+            public static float SignedAngle(Vector3 from, Vector3 to, Vector3 axis)
+            {
+                float unsignedAngle = Vector3.Angle(from, to);
+
+                float cross_x = from.y * to.z - from.z * to.y;
+                float cross_y = from.z * to.x - from.x * to.z;
+                float cross_z = from.x * to.y - from.y * to.x;
+                float sign = Mathf.Sign(axis.x * cross_x + axis.y * cross_y + axis.z * cross_z);
+                return unsignedAngle * sign;
+            }
+
+            public static bool Prefix(vgCompass __instance, Vector3 ___newRotation, float ___worldOffset)
+            {
+                var transform = __instance.transform;
+                var forward = Vector3.ProjectOnPlane(-transform.parent.forward, Vector3.up);
+                var angle = SignedAngle(forward, Vector3.forward, Vector3.up);
+                ___newRotation.y = angle - 165f - ___worldOffset;
+                transform.localEulerAngles = ___newRotation;
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(vgCompass), "Start")]
+        public class PatchCompassStart
+        {
+            private static void CreateLine(Color color, Transform parent, Vector3 destination)
+            {
+                var line = new GameObject("debugLine").transform;
+                line.transform.SetParent(parent, false);
+
+                var lineRenderer = line.gameObject.AddComponent<LineRenderer>();
+                lineRenderer.useWorldSpace = false;
+                lineRenderer.SetPositions(new[] { Vector3.zero, destination });
+                lineRenderer.startWidth = 0.005f;
+                lineRenderer.endWidth = 0.005f;
+                lineRenderer.endColor = color;
+                lineRenderer.startColor = color;
+                lineRenderer.material.shader = Shader.Find("Particles/Alpha Blended Premultiply");
+                lineRenderer.material.SetColor("_Color", color);
+            }
+
+            public static void Postfix(vgCompass __instance)
+            {
+                var transform = __instance.transform.parent;
+                CreateLine(Color.red, transform, Vector3.right);
+                CreateLine(Color.green, transform, Vector3.forward);
+                CreateLine(Color.blue, transform, Vector3.up);
             }
         }
     }
