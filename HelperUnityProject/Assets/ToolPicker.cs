296523c4dc1e3d8580bdd3c724b1f90c7d8f60a2
Raicuparta <raicuparta@gmail.com>
2021-06-21 22:34:46 +0200

    Start item picker in Unity Project

---
+++ HelperUnityProject/Assets/ToolPicker.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ToolPicker : MonoBehaviour {
+	private bool isOpen = false;
+	private MeshRenderer orbRenderer;
+	private List<ToolPickerItem> tools = new List<ToolPickerItem>();
+	private SteamVR_Action_Boolean input = SteamVR_Actions.default_Flashlight;
+	private ToolPickerItem selectedTool;
+
+	public Transform ParentWhileActive;
+	public Transform ParentWhileInactive;
+	public Transform ToolsContainer;
+	public Transform Hand;
+
+	private void Awake () {
+		orbRenderer = GetComponent<MeshRenderer>();
+
+		foreach (Transform child in ToolsContainer)
+        {
+			var tool = child.GetComponent<ToolPickerItem>();
+			if (tool) tools.Add(tool);
+        }
+	}
+
+	private float GetSquareDistance(Vector3 pointA, Vector3 pointB)
+    {
+		return (pointA - pointB).sqrMagnitude;
+    }
+
+	private float GetDistanceToHand(Transform compareTransform)
+    {
+		return GetSquareDistance(compareTransform.position, Hand.position);
+    }
+
+	private void UpdateSelectedTool()
+    {
+		var nextSelectedTool = tools[0];
+		var selectedToolDistance = GetDistanceToHand(nextSelectedTool.transform);
+
+		for (int i = 1; i < tools.Count; i++)
+        {
+			var tool = tools[i];
+			var distance = GetDistanceToHand(tool.transform);
+			if (distance < selectedToolDistance)
+            {
+				nextSelectedTool = tool;
+				selectedToolDistance = distance;
+			}
+        }
+		if (selectedTool)
+        {
+			selectedTool.Deselect();
+        }
+		selectedTool = nextSelectedTool;
+		nextSelectedTool.Select();
+    }
+	
+	private void Update () {
+		if (input.state)
+        {
+			UpdateSelectedTool();
+		}
+		if (input.stateDown)
+        {
+			ShowToolPicker();
+		}
+		if (input.stateUp)
+		{
+			HideToolPicker();
+		}
+	}
+
+	private void ShowToolPicker()
+    {
+		ToolsContainer.gameObject.SetActive(true);
+		ToolsContainer.SetParent(ParentWhileActive);
+		ToolsContainer.LookAt(Camera.main.transform);
+	}
+
+	private void HideToolPicker()
+    {
+		ToolsContainer.gameObject.SetActive(false);
+		ToolsContainer.SetParent(ParentWhileInactive);
+		ToolsContainer.localPosition = Vector3.zero;
+		ToolsContainer.localRotation = Quaternion.identity;
+	}
+}
---
+++ HelperUnityProject/Assets/ToolPickerItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolPickerItem : MonoBehaviour
+{
+    private bool isSelected = false;
+
+	private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("collider " + collider);
+    }
+
+    public void Select()
+    {
+        if (isSelected)
+        {
+            return;
+        }
+
+        isSelected = true;
+        transform.localScale = Vector3.one * 1.5f;
+    }
+
+    public void Deselect()
+    {
+        if (!isSelected)
+        {
+            return;
+        }
+
+        isSelected = false;
+        transform.localScale = Vector3.one;
+    }
+}
