a7f74c0be3062ec368bddfce81af46ffc45c879d
Raicuparta <raicuparta@gmail.com>
2021-07-02 09:13:58 +0200

    Axis

--- TwoForksVR/Input/VRInputManager.cs
+++ TwoForksVR/Input/VRInputManager.cs
@@ -2,8 +2,6 @@
 using MelonLoader;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 using Valve.VR;
 
@@ -12,16 +10,12 @@ namespace Raicuparta.TwoForksVR
     public class VRInputManager : MonoBehaviour
     {
         private static SteamVR_Input_ActionSet_default actionSet;
-        private static Dictionary<vgInputManager.InputDelegate, SteamVR_Action_Boolean.ChangeHandler> booleanDelegateHandlerMap = new Dictionary<vgInputManager.InputDelegate, SteamVR_Action_Boolean.ChangeHandler>();
-        private static Dictionary<vgInputManager.InputDelegate, SteamVR_Action_Vector2.ChangeHandler> vector2DelegateHandlerMap = new Dictionary<vgInputManager.InputDelegate, SteamVR_Action_Vector2.ChangeHandler>();
-
-
         private static Dictionary<string, SteamVR_Action_Boolean> booleanActionMap;
+        private static Dictionary<string, SteamVR_Action_Vector2> vector2XActionMap;
+        private static Dictionary<string, SteamVR_Action_Vector2> vector2YActionMap;
 
-        private void Start()
+        private static void Initialize()
         {
-            //DontDestroyOnLoad(gameObject);
-
             actionSet = SteamVR_Actions._default;
             booleanActionMap = new Dictionary<string, SteamVR_Action_Boolean>()
             {
@@ -31,238 +25,44 @@ namespace Raicuparta.TwoForksVR
                 { InputThing.Jog, actionSet.Jog },
                 { InputThing.Pause, actionSet.Cancel },
             };
-
-            // TODO relative path
-            MelonLogger.Msg("$$$$$ gonna try to");
-            try
+            vector2XActionMap = new Dictionary<string, SteamVR_Action_Vector2>()
             {
-            var error = OpenVR.Input.SetActionManifestPath(@"C:\Users\rai\Repos\two-forks-vr\TwoForksVR\Input\Bindings\actions.json");
-            MelonLogger.Msg("???? error ???? " + error);
-
-            } catch (Exception error)
+                { InputThing.MoveStrafe, actionSet.Move },
+                { InputThing.LookHorizontal, actionSet.Rotate },
+            };
+            vector2YActionMap = new Dictionary<string, SteamVR_Action_Vector2>()
             {
-                MelonLogger.Msg("????? exception ???? " + error);
-            }
+                { InputThing.MoveForward, actionSet.Move },
+                { InputThing.LookVertical, actionSet.Rotate },
+            };
+            return;
         }
-
-        //[HarmonyPatch(typeof(vgInputManager), "RegisterForInputCallback")]
-        //public class PatchRegisterInput
-        //{
-        //    private static SteamVR_Action_Boolean.ChangeHandler OnChangeBoolean(vgInputManager.InputDelegate callback)
-        //    {
-        //        return (SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState) =>
-        //        {
-        //            if (newState)
-        //            {
-        //                callback();
-        //            }
-        //        };
-        //    }
-
-        //    private static SteamVR_Action_Vector2.ChangeHandler OnChangeVector2Horizontal(vgInputManager.InputDelegate callback)
-        //    {
-        //        return (SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta) =>
-        //        {
-        //            callback(axis.x);
-        //        };
-        //    }
-
-        //    private static SteamVR_Action_Vector2.ChangeHandler OnChangeVector2Vertical(vgInputManager.InputDelegate callback)
-        //    {
-        //        return (SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta) =>
-        //        {
-        //            callback(axis.y);
-        //        };
-        //    }
-
-        //    public static bool Prefix(string commandName, vgInputManager.InputDelegate callback)
-        //    {
-        //        var command = (InputCommand)Enum.Parse(typeof(InputCommand), commandName);
-        //        var actionSet = SteamVR_Actions._default;
-
-        //        switch (command)
-        //        {
-        //            case InputCommand.UIClick:
-        //            case InputCommand.UISubmit:
-        //            case InputCommand.Use:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.Interact.onChange += OnChangeBoolean(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.MoveForward:
-        //            {
-        //                var changeHandler = OnChangeVector2Vertical(callback);
-        //                vector2DelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.Move.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.UIHorizontal:
-        //            case InputCommand.MoveStrafe:
-        //            {
-        //                var changeHandler = OnChangeVector2Horizontal(callback);
-        //                vector2DelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.Move.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.LookHorizontal_Stick:
-        //            {
-        //                var changeHandler = OnChangeVector2Horizontal(callback);
-        //                vector2DelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.Rotate.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.NextMenu:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.NextPage.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.PreviousMenu:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.PreviousPage.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.UICancel:
-        //            case InputCommand.Pause:
-        //            case InputCommand.UnPause:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.Cancel.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.ToggleJog:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.Jog.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.UIUp:
-        //            case InputCommand.DialogSelectionUp:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.UIUp.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            case InputCommand.UIDown:
-        //            case InputCommand.DialogSelectionDown:
-        //            {
-        //                var changeHandler = OnChangeBoolean(callback);
-        //                booleanDelegateHandlerMap.Add(callback, changeHandler);
-        //                actionSet.UIDown.onChange += changeHandler;
-        //                return false;
-        //            }
-        //            default:
-        //            {
-        //                return true;
-        //            }
-        //        }
-        //    }
-
-        //}
-
-        //[HarmonyPatch(typeof(vgInputManager), "UnregisterForInputCallback")]
-        //public class PatchUnregisterInput
-        //{
-        //    public static bool Prefix(string commandName, vgInputManager.InputDelegate callback)
-        //    {
-        //        var command = (InputCommand)Enum.Parse(typeof(InputCommand), commandName);
-        //        var actionSet = SteamVR_Actions._default;
-
-        //        switch (command)
-        //        {
-        //            case InputCommand.UIClick:
-        //            case InputCommand.UISubmit:
-        //            case InputCommand.Use:
-        //            {
-        //                actionSet.Interact.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.MoveForward:
-        //            {
-        //                actionSet.Move.onChange -= vector2DelegateHandlerMap[callback];
-        //                vector2DelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.UIHorizontal:
-        //            case InputCommand.MoveStrafe:
-        //            {
-        //                actionSet.Move.onChange -= vector2DelegateHandlerMap[callback];
-        //                vector2DelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.LookHorizontal_Stick:
-        //            {
-        //                actionSet.Rotate.onChange -= vector2DelegateHandlerMap[callback];
-        //                vector2DelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.NextMenu:
-        //            {
-        //                actionSet.NextPage.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.PreviousMenu:
-        //            {
-        //                actionSet.PreviousPage.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.UICancel:
-        //            case InputCommand.Pause:
-        //            case InputCommand.UnPause:
-        //            {
-        //                actionSet.Cancel.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.ToggleJog:
-        //            {
-        //                actionSet.Jog.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.UIUp:
-        //            case InputCommand.DialogSelectionUp:
-        //            {
-        //                actionSet.UIUp.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            case InputCommand.UIDown:
-        //            case InputCommand.DialogSelectionDown:
-        //            {
-        //                actionSet.UIDown.onChange -= booleanDelegateHandlerMap[callback];
-        //                booleanDelegateHandlerMap.Remove(callback);
-        //                return false;
-        //            }
-        //            default:
-        //            {
-        //                return true;
-        //            }
-        //        }
-        //    }
-        //}
 
         [HarmonyPatch(typeof(vgAxisData), "Update")]
         public class PatchKeyDataUpdate
         {
+
             public static void Postfix(List<string> ___names, ref float ___axisValue, ref float ___axisValueLastFrame)
             {
                 if (actionSet == null)
                 {
-                    actionSet = SteamVR_Actions._default;
-                    return;
+                    Initialize();
                 }
+
+                foreach (var name in ___names)
+                {
+                    if (vector2XActionMap.ContainsKey(name))
+                    {
+                        ___axisValue = vector2XActionMap[name].axis.x;
+                        ___axisValueLastFrame = vector2XActionMap[name].lastAxis.x;
+                    }
+                    if (vector2YActionMap.ContainsKey(name))
+                    {
+                        ___axisValue = vector2YActionMap[name].axis.y;
+                        ___axisValueLastFrame = vector2YActionMap[name].lastAxis.y;
+                    }
+                }
+
 
                 switch(___names[0])
                 {
@@ -293,19 +93,9 @@ namespace Raicuparta.TwoForksVR
 
             )
             {
-                if (booleanActionMap == null)
+                if (actionSet == null)
                 {
-                    MelonLogger.Msg("### null "); 
-                    actionSet = SteamVR_Actions._default;
-                    booleanActionMap = new Dictionary<string, SteamVR_Action_Boolean>()
-                    {
-                        { InputThing.Climb, actionSet.Interact },
-                        { InputThing.ChooseUp, actionSet.UIUp },
-                        { InputThing.ChooseDown, actionSet.UIDown },
-                        { InputThing.Jog, actionSet.Jog },
-                        { InputThing.Pause, actionSet.Cancel },
-                    };
-                    return;
+                    Initialize();
                 }
 
                 foreach (var name in ___names)
