f545e5202695ea3f8fc31051eb2d88fc5bedee70
Raicuparta <raicuparta@gmail.com>
2021-07-03 14:17:03 +0200

    Disable cursor manager

---
+++ TwoForksVR/Input/MousePatches.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Raicuparta.TwoForksVR
+{
+    public class MousePatches : MonoBehaviour
+    {
+        [HarmonyPatch(typeof(vgCursorManager), "Awake")]
+        public class PatchCursorManager
+        {
+            public static bool Prefix(vgCursorManager __instance)
+            {
+                Destroy(__instance);
+                return false;
+            }
+        }
+    }
+}
