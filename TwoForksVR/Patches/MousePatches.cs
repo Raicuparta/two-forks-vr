54d351d8755451800fff4a75520f4836397a39cb
Raicuparta <raicuparta@gmail.com>
2021-07-08 21:13:01 +0200

    Move MousePatches

---
+++ TwoForksVR/Patches/MousePatches.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+
+namespace TwoForksVR.Patches
+{
+    [HarmonyPatch(typeof(vgCursorManager), "Awake")]
+    public class PatchCursorManager
+    {
+        public static bool Prefix(vgCursorManager __instance)
+        {
+            UnityEngine.Object.Destroy(__instance);
+            return false;
+        }
+    }
+}
