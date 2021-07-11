bae9e5a9c954944e10be383b5cb4974e4c4f99ce
Raicuparta <raicuparta@gmail.com>
2021-07-11 16:43:14 +0200

    Cleanup canvas to world

--- TwoForksVR/UI/Patches/UIPatches.cs
+++ TwoForksVR/UI/Patches/UIPatches.cs
@@ -25,7 +25,7 @@ namespace TwoForksVR.UI
         private static readonly string[] canvasesToDisable =
         {
             "BlackBars", // Cinematic black bars.
-            "Camera", // Disposable camera.
+            "Camera", // Disposable camera. TODO: show this information in some other way.
         };
         private static readonly string[] canvasesToIgnore =
 {
@@ -39,31 +39,26 @@ namespace TwoForksVR.UI
                 return;
             }
 
-            var transform = __instance.transform;
+            var canvas = __instance.GetComponent<Canvas>();
 
+            if (canvasesToDisable.Contains(canvas.name))
+            {
+                canvas.enabled = false;
+                return;
+            }
+
+            if (canvas.renderMode != RenderMode.ScreenSpaceOverlay)
+            {
+                return;
+            }
+
+            var transform = __instance.transform;
             transform.SetParent(Camera.main.transform, false);
             transform.localPosition = Vector3.forward * 0.5f;
             transform.localScale = Vector3.one * 0.0004f;
-            var canvases = transform.GetComponentsInChildren<Canvas>(true).Where(canvas => canvas.renderMode == RenderMode.ScreenSpaceOverlay);
-            canvases.Do(canvas =>
-            {
-                if (canvasesToDisable.Contains(canvas.name))
-                {
-                    canvas.enabled = false;
-                    return;
-                }
-                canvas.worldCamera = Camera.main;
-                canvas.renderMode = RenderMode.WorldSpace;
-                canvas.transform.localPosition = Vector3.zero;
-                canvas.transform.localRotation = Quaternion.identity;
-                canvas.transform.localScale = Vector3.one;
 
-                if (canvas.name == "Camera")
-                {
-                    canvas.transform.SetParent(VRHandsManager.Instance.RightHand);
-                    canvas.transform.localScale = Vector3.one * 0.3f;
-                }
-            });
+            canvas.worldCamera = Camera.main;
+            canvas.renderMode = RenderMode.WorldSpace;
         }
     }
 }
