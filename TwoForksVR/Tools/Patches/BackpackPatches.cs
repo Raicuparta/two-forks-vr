43a3c9a96a467b80060fc8e18df2d6cbcad59b49
Raicuparta <raicuparta@gmail.com>
2021-08-02 20:54:41 +0200

    Prevent backpack becoming visible

---
+++ TwoForksVR/Tools/Patches/BackpackPatches.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TwoForksVR.Tools
+{
+    [HarmonyPatch(typeof(vgPlayerController), "SetBackpackVisibility")]
+    public class PreventShowingBackpack
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
