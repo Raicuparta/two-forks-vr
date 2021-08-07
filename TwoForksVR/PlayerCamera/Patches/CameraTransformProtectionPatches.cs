f73f98fac522a1c39d1e14288e36c7c13f311c46
Raicuparta <raicuparta@gmail.com>
2021-08-07 20:20:36 +0200

    Cleanup camera transform protection patches

--- TwoForksVR/PlayerCamera/Patches/CameraTransformProtectionPatches.cs
+++ TwoForksVR/PlayerCamera/Patches/CameraTransformProtectionPatches.cs
@@ -6,37 +6,21 @@ using UnityEngine;
 // All these patches try to disable any game code that would otherwise mess with the camera's transform values.
 namespace TwoForksVR.PlayerCamera
 {
-    [HarmonyPatch(typeof(vgCameraController), "ApplyPostAnimationTransform")]
-    public class DisableGameCameraMovement
+    [HarmonyPatch]
+    public class CameraTransformProtectionPatches
     {
-        public static bool Prefix()
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(vgCameraController), "ApplyPostAnimationTransform")]
+        [HarmonyPatch(typeof(vgCameraController), "UpdateFOV")]
+        [HarmonyPatch(typeof(vgCameraController), "UpdateClipPlaneOffset")]
+        public static bool DisableCameraModifications()
         {
             return false;
         }
-    }
 
-    [HarmonyPatch(typeof(vgCameraController), "UpdateFOV")]
-    public class DisableCameraUpdateFOV
-    {
-        public static bool Prefix()
-        {
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(vgCameraController), "UpdateClipPlaneOffset")]
-    public class DisableCameraClipPlaneOffset
-    {
-        public static bool Prefix()
-        {
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(vgCameraController), "LateUpdate")]
-    public class DisableCameraLateUpdate
-    {
-        public static bool Prefix()
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(vgCameraController), "LateUpdate")]
+        public static bool DisableCameraLateUpdate()
         {
             // If I always disable this method, it will break the camera position.
             // Since it was only broken while paused, I'm disabling it only in that scenario.
