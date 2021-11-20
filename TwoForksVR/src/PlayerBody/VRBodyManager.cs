f280ab24f3020c210bec8950ac3c43b480b02ffb
Raicuparta <raicuparta@gmail.com>
2021-11-21 00:05:43 +0100

    Better

--- TwoForksVR/src/PlayerBody/VRBodyManager.cs
+++ TwoForksVR/src/PlayerBody/VRBodyManager.cs
@@ -50,10 +50,6 @@ namespace TwoForksVr.PlayerBody
                 characterController = playerController.characterController;
             }
             UpdateRoomScalePosition();
-        }
-
-        private void LateUpdate()
-        {
             UpdateRotation();
         }
 
