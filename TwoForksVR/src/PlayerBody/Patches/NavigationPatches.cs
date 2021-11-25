32ab045d0fa038cd81f8618df43050b7db789ebc
Raicuparta <raicuparta@gmail.com>
2021-11-25 16:14:03 +0100

    Prevent rotating during animations

--- TwoForksVR/src/PlayerBody/Patches/NavigationPatches.cs
+++ TwoForksVR/src/PlayerBody/Patches/NavigationPatches.cs
@@ -13,8 +13,7 @@ namespace TwoForksVr.PlayerBody.Patches
             // Usually NavigationController uses player camera forward as a basis for movement direction.
             // This dummy camera is used instead, so that NavigationController now only uses the player body direction.
             var dummyCamera = new GameObject("ForwardFacingDummyCamera").AddComponent<Camera>();
-            // TODO dont use child index. It's because in VRBodyManager I'm using transform.parent.Rotate.
-            dummyCamera.transform.SetParent(__instance.transform.GetChild(0), false);
+            dummyCamera.transform.SetParent(__instance.transform, false);
             dummyCamera.enabled = false;
             __instance.playerCamera = dummyCamera;
         }
