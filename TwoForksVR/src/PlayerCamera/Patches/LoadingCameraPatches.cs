588e2d0e7df502b6e59785bd5d6b042fb369a08b
Raicuparta <raicuparta@gmail.com>
2021-11-27 16:16:46 +0100

    Skip vgLoadingCamera behavior

---
+++ TwoForksVR/src/PlayerCamera/Patches/LoadingCameraPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace TwoForksVr.PlayerCamera.Patches
+{    
+    [HarmonyPatch]
+    public class LoadingCameraPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(vgLoadingCamera), nameof(vgLoadingCamera.OnEnable))]
+        private static bool DisableVgLoadingCamera(vgLoadingCamera __instance)
+        {
+            __instance.enabled = false;
+            return false;
+        }
+        
+        [HarmonyPatch(typeof(vgLoadingCamera), nameof(vgLoadingCamera.OnDestroy))]
+        [HarmonyPatch(typeof(vgLoadingCamera), nameof(vgLoadingCamera.OnDisable))]
+        private static bool SkipLoadingCameraDisableAndDestroy()
+        {
+            return false;
+        }
+    }
+}
