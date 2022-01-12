b035315452bd55ad8f859c5cf742496f68a3e4ab
Raicuparta <raicuparta@gmail.com>
2022-01-13 00:03:30 +0100

    Comment

--- TwoForksVr/src/Limbs/VrHand.cs
+++ TwoForksVr/src/Limbs/VrHand.cs
@@ -7,6 +7,7 @@ namespace TwoForksVr.Limbs
 {
     public class VrHand : MonoBehaviour
     {
+        // TODO make hand and foot extend from Limb
         private GameObject fallbackHandModel;
         private string handName;
         private bool isLeft;
