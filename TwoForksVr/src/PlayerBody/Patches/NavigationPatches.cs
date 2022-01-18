6131203828bc94c9f7b8b723397b2ba35e8dc52e
Raicuparta <raicuparta@gmail.com>
2022-01-18 16:07:31 +0100

    Fix flying, closes #80

--- TwoForksVr/src/PlayerBody/Patches/NavigationPatches.cs
+++ TwoForksVr/src/PlayerBody/Patches/NavigationPatches.cs
@@ -1,26 +1,22 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace TwoForksVr.PlayerBody.Patches
 {
     [HarmonyPatch]
     public static class NavigationPatches
     {
-        // TODO: use this patch for controller based movement direction.
-        // [HarmonyPostfix]
-        // [HarmonyPatch(typeof(vgPlayerNavigationController), nameof(vgPlayerNavigationController.Start))]
-        // public static void CreateBodyManager(vgPlayerNavigationController __instance)
-        // {
-        //     // Usually NavigationController uses player camera forward as a basis for movement direction.
-        //     // This dummy camera is used instead, so that NavigationController now only uses the player body direction.
-        //     var dummyCamera = new GameObject("ForwardFacingDummyCamera").AddComponent<Camera>();
-        //     dummyCamera.transform.SetParent(__instance.transform, false);
-        //     dummyCamera.enabled = false;
-        //     __instance.playerCamera = dummyCamera;
-        //
-        //     __instance.playerRotationSpringConstant = 0;
-        //     __instance.playerRotationDamping = 0;
-        //     __instance.largestAllowedYawDelta = 0;
-        // }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(vgPlayerNavigationController), nameof(vgPlayerNavigationController.Start))]
+        public static void CreateBodyManager(vgPlayerNavigationController __instance)
+        {
+            // Usually NavigationController uses player camera forward as a basis for movement direction.
+            // This dummy camera is used instead, so that movement direction can be independent of the camera rotation.
+            var dummyCamera = new GameObject("ForwardFacingDummyCamera").AddComponent<Camera>();
+            dummyCamera.transform.SetParent(__instance.transform.Find("henry"), false);
+            dummyCamera.enabled = false;
+            __instance.playerCamera = dummyCamera;
+        }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(vgPlayerNavigationController), nameof(vgPlayerNavigationController.Start))]
