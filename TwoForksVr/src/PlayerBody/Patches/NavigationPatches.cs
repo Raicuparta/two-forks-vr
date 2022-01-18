adce4352325e744ed4abd8a7ab1c37b574408d66
Raicuparta <raicuparta@gmail.com>
2022-01-18 14:28:04 +0100

    Fix movement direction

--- TwoForksVr/src/PlayerBody/Patches/NavigationPatches.cs
+++ TwoForksVr/src/PlayerBody/Patches/NavigationPatches.cs
@@ -1,26 +1,26 @@
 ﻿using HarmonyLib;
-using UnityEngine;
 
 namespace TwoForksVr.PlayerBody.Patches
 {
     [HarmonyPatch]
     public static class NavigationPatches
     {
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(vgPlayerNavigationController), nameof(vgPlayerNavigationController.Start))]
-        public static void CreateBodyManager(vgPlayerNavigationController __instance)
-        {
-            // Usually NavigationController uses player camera forward as a basis for movement direction.
-            // This dummy camera is used instead, so that NavigationController now only uses the player body direction.
-            var dummyCamera = new GameObject("ForwardFacingDummyCamera").AddComponent<Camera>();
-            dummyCamera.transform.SetParent(__instance.transform, false);
-            dummyCamera.enabled = false;
-            __instance.playerCamera = dummyCamera;
-
-            __instance.playerRotationSpringConstant = 0;
-            __instance.playerRotationDamping = 0;
-            __instance.largestAllowedYawDelta = 0;
-        }
+        // TODO: use this patch for controller based movement direction.
+        // [HarmonyPostfix]
+        // [HarmonyPatch(typeof(vgPlayerNavigationController), nameof(vgPlayerNavigationController.Start))]
+        // public static void CreateBodyManager(vgPlayerNavigationController __instance)
+        // {
+        //     // Usually NavigationController uses player camera forward as a basis for movement direction.
+        //     // This dummy camera is used instead, so that NavigationController now only uses the player body direction.
+        //     var dummyCamera = new GameObject("ForwardFacingDummyCamera").AddComponent<Camera>();
+        //     dummyCamera.transform.SetParent(__instance.transform, false);
+        //     dummyCamera.enabled = false;
+        //     __instance.playerCamera = dummyCamera;
+        //
+        //     __instance.playerRotationSpringConstant = 0;
+        //     __instance.playerRotationDamping = 0;
+        //     __instance.largestAllowedYawDelta = 0;
+        // }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(vgPlayerNavigationController), nameof(vgPlayerNavigationController.Start))]
