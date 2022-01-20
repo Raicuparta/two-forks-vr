94b4330d349319ca9e30e7aea79781423759a570
Raicuparta <raicuparta@gmail.com>
2022-01-20 16:43:18 +0100

    Cleanup

--- TwoForksVr/src/PlayerCamera/Patches/CameraTransformProtectionPatches.cs
+++ TwoForksVr/src/PlayerCamera/Patches/CameraTransformProtectionPatches.cs
@@ -13,8 +13,6 @@ namespace TwoForksVr.PlayerCamera.Patches
         [HarmonyPatch(typeof(vgCameraController), nameof(vgCameraController.ApplyPostAnimationTransform))]
         [HarmonyPatch(typeof(vgCameraController), nameof(vgCameraController.UpdateFOV))]
         [HarmonyPatch(typeof(vgCameraController), nameof(vgCameraController.UpdateClipPlaneOffset))]
-        [HarmonyPatch(typeof(vgCameraTargetSource), nameof(vgCameraTargetSource.UpdateLookAt))]
-        [HarmonyPatch(typeof(vgCameraTargetSource), nameof(vgCameraTargetSource.UpdateAnimation))]
         private static bool DisableCameraModifications()
         {
             return false;
