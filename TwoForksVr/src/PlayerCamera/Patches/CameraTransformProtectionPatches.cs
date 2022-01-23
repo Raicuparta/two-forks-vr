30ea1fdd5c807d67f329997b0a2040a67ee2c299
Raicuparta <raicuparta@gmail.com>
2022-01-23 17:33:59 +0100

    Add option for fixed camera during animations

--- TwoForksVr/src/PlayerCamera/Patches/CameraTransformProtectionPatches.cs
+++ TwoForksVr/src/PlayerCamera/Patches/CameraTransformProtectionPatches.cs
@@ -38,6 +38,11 @@ namespace TwoForksVr.PlayerCamera.Patches
         [HarmonyPatch(typeof(vgCameraController), nameof(vgCameraController.UpdatePosition))]
         private static bool TeleportPosition(vgCameraController __instance)
         {
+            if (VrSettings.FixedCameraDuringAnimations.Value && !__instance.playerController.navController.enabled)
+            {
+                return false;
+            }
+            
             var hasReachedTeleportMarked = TeleportArc.IsNextToTeleportMarker(__instance.playerController.transform);
 
             if (hasReachedTeleportMarked && TeleportArc.IsTeleporting())
@@ -52,6 +57,11 @@ namespace TwoForksVr.PlayerCamera.Patches
         [HarmonyPatch(typeof(vgCameraController), nameof(vgCameraController.UpdateCameraStack))]
         private static bool PreventRotationWhileTeleporting(vgCameraController __instance)
         {
+            if (!__instance.playerController.navController.enabled)
+            {
+                return false;
+            }
+
             return !TeleportArc.IsTeleporting() || TeleportArc.IsNextToTeleportMarker(__instance.playerController.transform);
         }
     }
--- TwoForksVr/src/Settings/VrSettings.cs
+++ TwoForksVr/src/Settings/VrSettings.cs
@@ -5,25 +5,25 @@ namespace TwoForksVr.Settings
     public static class VrSettings
     {
         public static ConfigFile Config;
-        // public static ConfigEntry<bool> HandOrientedMovement;
         public static ConfigEntry<bool> SnapTurning;
         public static ConfigEntry<bool> ShowFeet;
         public static ConfigEntry<bool> ShowBody;
         public static ConfigEntry<bool> Teleport;
+        public static ConfigEntry<bool> FixedCameraDuringAnimations;
 
         public static void SetUp(ConfigFile config)
         {
             Config = config;
-            // HandOrientedMovement = config.Bind("Config", "HandOrientedMovement", false,
-            //     "Hand oriented movement");
             SnapTurning = config.Bind("Config", "SnapTurning", false,
                 "Snap turning");
             ShowBody = config.Bind("Config", "ShowBody", false,
                 "Show player body");
             ShowFeet = config.Bind("Config", "ShowFeet", true,
                 "Show player feet");
-            Teleport = config.Bind("Config", "Teleport", true,
+            Teleport = config.Bind("Config", "Teleport", false,
                 "Fixed camera (teleport)");
+            FixedCameraDuringAnimations = config.Bind("Config", "FixedCameraDuringAnimations", false,
+                "Fixed camera during player animations");
         }
     }
 }
