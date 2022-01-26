beb3cd93f72f7af006d0fbff821986535f4a9ead
Raicuparta <raicuparta@gmail.com>
2022-01-27 00:00:04 +0100

    Cleanup

--- TwoForksVr/src/UI/InteractiveUiTarget.cs
+++ TwoForksVr/src/UI/InteractiveUiTarget.cs
@@ -4,62 +4,24 @@ using UnityEngine;
 
 namespace TwoForksVr.UI
 {
-    // This is an invisible object that's always(ish) somewhere in front of the player.
-    // To be used as the position for UI elements that need to be visible or interacted with.
-    public class InteractiveUiTarget : MonoBehaviour
+    public class InteractiveUiTarget : UiTarget
     {
-        private const float forwardOffset = 3f;
-        private const float rotationSmoothTime = 0.3f;
-        private const float minAngleDelta = 45f;
-        private Transform cameraTransform;
-        private Vector3 previousForward;
-        private Quaternion rotationVelocity;
-        private Quaternion targetRotation;
-        private Transform targetTransform;
+        protected override float MinAngleDelta => 45f;
 
         public static InteractiveUiTarget Create(VrStage stage)
         {
             var instance = new GameObject(nameof(InteractiveUiTarget)).AddComponent<InteractiveUiTarget>();
             instance.transform.SetParent(stage.transform, false);
-            instance.targetTransform = new GameObject("InteractiveUiTargetTransform").transform;
-            instance.targetTransform.SetParent(instance.transform, false);
-            instance.targetTransform.localPosition = Vector3.forward * forwardOffset;
-            InteractiveUi.SetTargetTransform(instance.targetTransform);
+            instance.TargetTransform = new GameObject("InteractiveUiTargetTransform").transform;
+            instance.TargetTransform.SetParent(instance.transform, false);
+            instance.TargetTransform.localPosition = Vector3.forward * 3f;
+            InteractiveUi.SetTargetTransform(instance.TargetTransform);
             return instance;
         }
 
-        public void SetUp(Camera camera)
+        protected override Vector3 GetCameraForward()
         {
-            if (!camera) return;
-            cameraTransform = camera.transform;
-            previousForward = MathHelper.GetProjectedForward(cameraTransform);
-        }
-
-        private void Update()
-        {
-            UpdateTransform();
-        }
-
-        private void UpdateTransform()
-        {
-            if (!cameraTransform) return;
-
-            var cameraForward = MathHelper.GetProjectedForward(cameraTransform);
-            var unsignedAngleDelta = Vector3.Angle(previousForward, cameraForward);
-
-            if (unsignedAngleDelta > minAngleDelta)
-            {
-                targetRotation = Quaternion.LookRotation(cameraForward);
-                previousForward = cameraForward;
-            }
-
-            transform.rotation = MathHelper.SmoothDamp(
-                transform.rotation,
-                targetRotation,
-                ref rotationVelocity,
-                rotationSmoothTime);
-
-            transform.position = cameraTransform.position;
+            return MathHelper.GetProjectedForward(CameraTransform);
         }
     }
 }
--- TwoForksVr/src/UI/StaticUiTarget.cs
+++ TwoForksVr/src/UI/StaticUiTarget.cs
@@ -1,65 +1,26 @@
-using TwoForksVr.Helpers;
 using TwoForksVr.Stage;
 using UnityEngine;
 
 namespace TwoForksVr.UI
 {
-    // This is an invisible object that's always(ish) somewhere in front of the player.
-    // To be used as the position for UI elements that need to be visible or interacted with.
-    public class StaticUiTarget : MonoBehaviour
+    public class StaticUiTarget : UiTarget
     {
-        private const float forwardOffset = 1f;
-        private const float rotationSmoothTime = 0.3f;
-        private const float minAngleDelta = 15f;
-        private Transform cameraTransform;
-        private Vector3 previousForward;
-        private Quaternion rotationVelocity;
-        private Quaternion targetRotation;
-        private Transform targetTransform;
+        protected override float MinAngleDelta => 15f;
 
         public static StaticUiTarget Create(VrStage stage)
         {
             var instance = new GameObject(nameof(StaticUiTarget)).AddComponent<StaticUiTarget>();
             instance.transform.SetParent(stage.transform, false);
-            instance.targetTransform = new GameObject("InteractiveUiTargetTransform").transform;
-            instance.targetTransform.SetParent(instance.transform, false);
-            instance.targetTransform.localPosition = new Vector3(0, 0, 1f);
-            StaticUi.SetTargetTransform(instance.targetTransform);
+            instance.TargetTransform = new GameObject("InteractiveUiTargetTransform").transform;
+            instance.TargetTransform.SetParent(instance.transform, false);
+            instance.TargetTransform.localPosition = Vector3.forward;
+            StaticUi.SetTargetTransform(instance.TargetTransform);
             return instance;
         }
 
-        public void SetUp(Camera camera)
+        protected override Vector3 GetCameraForward()
         {
-            if (!camera) return;
-            cameraTransform = camera.transform;
-            previousForward = cameraTransform.forward;
-        }
-
-        private void Update()
-        {
-            UpdateTransform();
-        }
-
-        private void UpdateTransform()
-        {
-            if (!cameraTransform) return;
-
-            var cameraForward = cameraTransform.forward;
-            var unsignedAngleDelta = Vector3.Angle(previousForward, cameraForward);
-
-            if (unsignedAngleDelta > minAngleDelta)
-            {
-                targetRotation = Quaternion.LookRotation(cameraForward);
-                previousForward = cameraForward;
-            }
-
-            transform.rotation = MathHelper.SmoothDamp(
-                transform.rotation,
-                targetRotation,
-                ref rotationVelocity,
-                rotationSmoothTime);
-
-            transform.position = cameraTransform.position;
+            return CameraTransform.forward;
         }
     }
 }
