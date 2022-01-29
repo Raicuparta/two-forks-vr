4b705834d202ab67c0677624f15b4360c501e093
Raicuparta <raicuparta@gmail.com>
2022-01-29 21:47:05 +0100

    Refactor

--- TwoForksVr/src/VrCamera/Patches/LoadingCameraPatches.cs
+++ TwoForksVr/src/VrCamera/Patches/LoadingCameraPatches.cs
@@ -6,8 +6,6 @@ namespace TwoForksVr.VrCamera.Patches
     [HarmonyPatch]
     public class LoadingCameraPatches
     {
-        private const string VrLoadingCameraName = "VrLoadingCamera";
-
         [HarmonyPrefix]
         [HarmonyPatch(typeof(vgLoadingCamera), nameof(vgLoadingCamera.OnDestroy))]
         [HarmonyPatch(typeof(vgLoadingCamera), nameof(vgLoadingCamera.OnDisable))]
@@ -24,28 +22,15 @@ namespace TwoForksVr.VrCamera.Patches
         {
             var canvas = __instance.transform.parent.GetComponent<Canvas>();
             canvas.transform.localPosition = Vector3.zero;
+
             // Move loading spinner from corner to center.
             var loadSpinner = canvas.transform.Find("LoadSpinner/UI_LoadSpinner/");
             var loadSpinnerPosition = loadSpinner.localPosition;
             loadSpinner.localPosition = new Vector3(0, -150, loadSpinnerPosition.z);
 
-            var mainGameFlowTransform = __instance.transform.parent.parent;
-            var onlyLoadOnceTransform = mainGameFlowTransform.transform.parent;
-            if (onlyLoadOnceTransform.Find(VrLoadingCameraName)) return;
+            if (Object.FindObjectOfType<VrLoadingCamera>()) return;
 
-            var parentCamera = new GameObject(VrLoadingCameraName).AddComponent<Camera>();
-            VrLoadingCamera.Create(__instance, parentCamera);
-            parentCamera.cullingMask = LayerMask.GetMask("UI");
-            parentCamera.transform.SetParent(onlyLoadOnceTransform, false);
-            parentCamera.clearFlags = CameraClearFlags.SolidColor;
-            parentCamera.backgroundColor = Color.black;
-
-            var parentCanvas = mainGameFlowTransform.gameObject.AddComponent<Canvas>();
-            parentCanvas.worldCamera = parentCamera;
-            parentCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-
-            var camera = __instance.GetComponent<Camera>();
-            camera.enabled = false;
+            VrLoadingCamera.Create(__instance);
         }
     }
 }
--- TwoForksVr/src/VrCamera/VrLoadingCamera.cs
+++ TwoForksVr/src/VrCamera/VrLoadingCamera.cs
@@ -8,14 +8,15 @@ namespace TwoForksVr.VrCamera
         private vgLoadingCamera loadingCamera;
         private Camera vrCamera;
 
-        public static void Create(vgLoadingCamera loadingCamera, Camera vrCamera)
+        public static void Create(vgLoadingCamera loadingCamera)
         {
-            var instance = vrCamera.gameObject.AddComponent<VrLoadingCamera>();
+            var gameObject = new GameObject("VrLoadingCamera");
+            var instance = gameObject.AddComponent<VrLoadingCamera>();
             instance.loadingCamera = loadingCamera;
-            var onlyLoadOnce = instance.gameObject.AddComponent<vgOnlyLoadOnce>();
-            onlyLoadOnce.dontDestroyOnLoad = true;
-            onlyLoadOnce.dontDestroyOnReset = true;
-            instance.vrCamera = vrCamera;
+            instance.SetUpVrCamera();
+            instance.SetUpOnlyLoadOnce();
+            instance.SetUpParentCanvas();
+            instance.DisableOriginalCamera();
         }
 
         private void LateUpdate()
@@ -27,6 +28,42 @@ namespace TwoForksVr.VrCamera
             }
 
             vrCamera.enabled = loadingCamera.isActiveAndEnabled;
+        }
+
+        private void DisableOriginalCamera()
+        {
+            var camera = loadingCamera.GetComponent<Camera>();
+            camera.enabled = false;
+        }
+
+        private void SetUpVrCamera()
+        {
+            vrCamera = gameObject.AddComponent<Camera>();
+            vrCamera.cullingMask = LayerMask.GetMask("UI");
+            vrCamera.clearFlags = CameraClearFlags.SolidColor;
+            vrCamera.backgroundColor = Color.black;
+        }
+
+        private void SetUpOnlyLoadOnce()
+        {
+            var onlyLoadOnce = gameObject.AddComponent<vgOnlyLoadOnce>();
+            onlyLoadOnce.dontDestroyOnLoad = true;
+            onlyLoadOnce.dontDestroyOnReset = true;
+        }
+
+        private void SetUpParentCanvas()
+        {
+            Logs.LogInfo($"SetUpParentCanvas loadingCamera {loadingCamera}");
+            Logs.LogInfo($"SetUpParentCanvas loadingCamera.transform {loadingCamera.transform}");
+            Logs.LogInfo($"SetUpParentCanvas loadingCamera.transform.parent {loadingCamera.transform.parent}");
+            Logs.LogInfo(
+                $"SetUpParentCanvas loadingCamera.transform.parent.parent {loadingCamera.transform.parent.parent}");
+            Logs.LogInfo(
+                $"SetUpParentCanvas loadingCamera.transform.parent.parent.gameObject {loadingCamera.transform.parent.parent.gameObject}");
+            var parentCanvas = loadingCamera.transform.parent.parent.gameObject.AddComponent<Canvas>();
+            Logs.LogInfo($"SetUpParentCanvas parentCanvas {parentCanvas}");
+            parentCanvas.worldCamera = vrCamera;
+            parentCanvas.renderMode = RenderMode.ScreenSpaceCamera;
         }
     }
 }
