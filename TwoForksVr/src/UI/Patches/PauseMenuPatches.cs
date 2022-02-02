057f70d31f1277bf3df772a7a21e758af0247826
Raicuparta <raicuparta@gmail.com>
2022-02-02 15:17:19 +0100

    Remove quit to menu option. Closes #129

---
+++ TwoForksVr/src/UI/Patches/PauseMenuPatches.cs
@@ -0,0 +1,33 @@
+using HarmonyLib;
+using TwoForksVr.Helpers;
+using UnityEngine;
+
+namespace TwoForksVr.UI.Patches
+{
+    [HarmonyPatch]
+    public static class PauseMenuPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(vgHudManager), nameof(vgHudManager.Awake))]
+        private static void RemoveQuitToMenuButton(vgHudManager __instance)
+        {
+            var quitToMenu =
+                __instance.pauseRoot.transform.Find("SafeZoner/Settings Menu Group/Quit Button");
+
+            if (quitToMenu == null)
+            {
+                Logs.LogWarning("Failed to find quit to menu button");
+                return;
+            }
+
+            Object.Destroy(quitToMenu.gameObject);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(vgScrimManager), nameof(vgScrimManager.ShowScrim))]
+        private static void DisablePauseBlur(ref bool blur)
+        {
+            blur = false;
+        }
+    }
+}
