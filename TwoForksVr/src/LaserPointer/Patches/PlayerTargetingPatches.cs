7825b6aa719f7f6c03f4b43e5026e1a25318996b
Raicuparta <raicuparta@gmail.com>
2022-02-05 14:32:08 +0100

    Add offset to targeting ray. Fixes #89

--- TwoForksVr/src/LaserPointer/Patches/PlayerTargetingPatches.cs
+++ TwoForksVr/src/LaserPointer/Patches/PlayerTargetingPatches.cs
@@ -6,6 +6,15 @@ namespace TwoForksVr.LaserPointer.Patches
     [HarmonyPatch]
     public class PlayerTargetingPatches : TwoForksVrPatch
     {
+        // Distance from targeting origin and the targetable object is used for deciding which interactions to allow.
+        // Some times, this makes it hard to target some objects with our hands if we're standing too close to them.
+        // This is because the original targeting origin was in the player's head, and now it's in the hand laser, which
+        // is naturally positioned a bit more forward than the head. 
+        // I tried a bunch of nice ways to fix this, but the only reliable solution was this hack, where I make the
+        // targeting ray start at an offset behind the laser, so that we have some more distance between the origin and
+        // the targets.
+        private const float targetingOriginOffset = 0.8f;
+
         [HarmonyPrefix]
         [HarmonyPatch(typeof(vgPlayerTargeting), nameof(vgPlayerTargeting.UpdateTarget))]
         private static void UserLaserForTargeting(ref Vector3 cameraFacing, ref Vector3 cameraOrigin)
@@ -13,7 +22,7 @@ namespace TwoForksVr.LaserPointer.Patches
             var laser = StageInstance.GetLaserTransform();
             if (!StageInstance.GetLaserTransform()) return;
             cameraFacing = laser.forward;
-            cameraOrigin = laser.position;
+            cameraOrigin = laser.position - laser.forward * targetingOriginOffset;
         }
     }
 }
