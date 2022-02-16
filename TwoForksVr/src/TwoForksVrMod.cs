cfd22d2f2959afa8b88e2d41136f4e230fcc79f2
Raicuparta <raicuparta@gmail.com>
2022-02-16 17:28:36 +0100

    Bump version 0.0.15

--- TwoForksVr/src/TwoForksVrMod.cs
+++ TwoForksVr/src/TwoForksVrMod.cs
@@ -6,7 +6,7 @@ using TwoForksVr.Settings;
 
 namespace TwoForksVr
 {
-    [BepInPlugin("raicuparta.twoforksvr", "Two Forks VR", "0.0.14")]
+    [BepInPlugin("raicuparta.twoforksvr", "Two Forks VR", "0.0.15")]
     public class TwoForksVrMod : BaseUnityPlugin
     {
         private void Awake()
