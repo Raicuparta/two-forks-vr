fc31e2520686782d5dc0b9077fb139c32f99663c
Raicuparta <raicuparta@gmail.com>
2022-03-14 11:09:52 +0100

    Add setting for snap turning

--- TwoForksVr/src/Settings/VrSettingsMenu.cs
+++ TwoForksVr/src/Settings/VrSettingsMenu.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using BepInEx.Configuration;
 using TwoForksVr.Assets;
 using TwoForksVr.Stage;
 using TwoForksVr.UI;
@@ -27,8 +30,11 @@ namespace TwoForksVr.Settings
 
             var content = scrollView.Find("Viewport/Content");
 
-            var toggleObject = scrollView.GetComponentInChildren<Toggle>(true).gameObject;
+            var toggleObject = content.GetComponentInChildren<Toggle>(true).gameObject;
             toggleObject.SetActive(false);
+
+            var dropdownObject = content.GetComponentInChildren<Dropdown>(true).gameObject;
+            dropdownObject.SetActive(false);
 
             var sectionObject = content.Find("Section").gameObject;
             sectionObject.SetActive(false);
@@ -44,30 +50,71 @@ namespace TwoForksVr.Settings
 
                 category.ForEach(configEntry =>
                 {
-                    var toggleInstance = Instantiate(toggleObject, sectionInstance.transform, false);
-                    toggleInstance.SetActive(true);
-
-                    var configEntryTextParts = configEntry.Value.Description.Description.Split('|');
-
-                    var label = toggleInstance.transform.Find("Label").GetComponent<Text>();
-                    label.text = configEntryTextParts[0];
-
-                    var description = toggleInstance.transform.Find("Description").GetComponent<Text>();
-                    if (configEntryTextParts.Length > 1)
-                        description.text = configEntryTextParts[1];
-                    else
-                        Destroy(description.gameObject);
-
-                    var toggleInput = toggleInstance.GetComponentInChildren<Toggle>();
-                    toggleInput.isOn = (bool) configEntry.Value.BoxedValue;
-
-                    toggleInput.onValueChanged.AddListener(isOn => { configEntry.Value.BoxedValue = isOn; });
+                    if (configEntry.Value.SettingType == typeof(bool))
+                        CreateToggle(configEntry, toggleObject, sectionInstance);
+                    else if (configEntry.Value.SettingType.IsEnum)
+                        CreateDropdown(configEntry, dropdownObject, sectionInstance);
                 });
             });
 
             instance.Close();
 
             return instance;
+        }
+
+        private static void CreateToggle(KeyValuePair<ConfigDefinition, ConfigEntryBase> configEntry,
+            GameObject toggleObject,
+            GameObject sectionInstance)
+        {
+            var toggleInstance = Instantiate(toggleObject, sectionInstance.transform, false);
+            toggleInstance.SetActive(true);
+
+            SetSettingText(toggleInstance, configEntry);
+
+            var toggleInput = toggleInstance.GetComponentInChildren<Toggle>();
+            toggleInput.isOn = (bool) configEntry.Value.BoxedValue;
+
+            toggleInput.onValueChanged.AddListener(isOn => { configEntry.Value.BoxedValue = isOn; });
+        }
+
+        private static void CreateDropdown(KeyValuePair<ConfigDefinition, ConfigEntryBase> configEntry,
+            GameObject dropdownObject,
+            GameObject sectionInstance)
+        {
+            var dropdownInstance = Instantiate(dropdownObject, sectionInstance.transform, false);
+            dropdownInstance.SetActive(true);
+
+            SetSettingText(dropdownInstance, configEntry);
+
+            var dropdownInput = dropdownInstance.GetComponentInChildren<Dropdown>();
+            var valueInSettings = (int) configEntry.Value.BoxedValue;
+            var enumValues = Enum.GetValues(configEntry.Value.SettingType);
+
+            dropdownInput.options.Clear();
+            foreach (var enumValue in enumValues)
+                dropdownInput.options.Add(
+                    new Dropdown.OptionData($"{configEntry.Value.Description.Description}: {(int) enumValue}"));
+            dropdownInput.value = Array.IndexOf(Enum.GetValues(configEntry.Value.SettingType), valueInSettings);
+
+            dropdownInput.onValueChanged.AddListener(value =>
+            {
+                configEntry.Value.BoxedValue = enumValues.GetValue(value);
+            });
+        }
+
+        private static void SetSettingText(GameObject settingInstance,
+            KeyValuePair<ConfigDefinition, ConfigEntryBase> configEntry)
+        {
+            var configEntryTextParts = configEntry.Value.Description.Description.Split('|');
+
+            var label = settingInstance.transform.Find("Label").GetComponent<Text>();
+            label.text = configEntryTextParts[0];
+
+            var description = settingInstance.transform.Find("Description").GetComponent<Text>();
+            if (configEntryTextParts.Length > 1)
+                description.text = configEntryTextParts[1];
+            else
+                Destroy(description.gameObject);
         }
 
         public void Open()
