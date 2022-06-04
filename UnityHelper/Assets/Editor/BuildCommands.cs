f9e338fb25b863c9a644dd3eb09c5521e976a29f
Raicuparta <Raicuparta@users.noreply.github.com>
2022-06-05 02:12:41 +0800

    Cleanup build process, use Unity editor instead of direct csproj build

---
+++ UnityHelper/Assets/Editor/BuildCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+
+/// <summary>
+/// Set of instructions for automated builds
+/// </summary>
+public static class BuildCommands
+{
+    static void PerformBuild ()
+    {
+        var buildPlayerOptions = new BuildPlayerOptions
+        {
+            locationPathName = "Build/UnityHelper.exe",
+            target = BuildTarget.StandaloneWindows64,
+            options = BuildOptions.None
+        };
+        EditorUserBuildSettings.development = false;
+
+        var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        EditorApplication.Exit(String.IsNullOrEmpty(buildReport) ? 0 : 1);
+    }
+}
